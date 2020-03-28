0a1a802ba70f7261c36f52c34f1d9b79c3c7ff3b
hwangikjae <hwangikjae@nate.com>
2020-03-28 20:49:15 +0900

    2020.03.28

---
+++ src/main/java/com/jojoIdu/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,36 @@
+package com.jojoIdu.book.springboot.domain.posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+
+public class Posts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String contents;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String contents, String author){
+        this.title = title;
+        this.contents = contents;
+        this.author = author;
+    }
+
+}
