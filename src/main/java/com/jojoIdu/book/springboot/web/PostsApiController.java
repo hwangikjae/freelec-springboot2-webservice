14c78b05e7f8888d114b11c728563b8a19f48d3a
hwangikjae <hwangikjae@nate.com>
2020-03-30 19:56:43 +0900

    2020.03.29

--- src/main/java/com/jojoIdu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoIdu/book/springboot/web/PostsApiController.java
@@ -1,10 +1,10 @@
 package com.jojoIdu.book.springboot.web;
 import com.jojoIdu.book.springboot.service.posts.PostsService;
+import com.jojoIdu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoIdu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jojoIdu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,4 +15,16 @@ public class PostsApiController {
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
     }
+
+    @PutMapping("/api/vi/post/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/vi/post/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+
+
 }
