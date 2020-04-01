87f6ddb09b4bc4f93093c6e4fecbf2f9b5079d0a
hwangikjae <hwangikjae@nate.com>
2020-04-01 20:17:55 +0900

    2020.04.01 mustache 세팅

--- src/main/java/com/jojoIdu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoIdu/book/springboot/domain/posts/Posts.java
@@ -1,4 +1,5 @@
 package com.jojoIdu.book.springboot.domain.posts;
+import com.jojoIdu.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import javax.persistence.Id;
 @NoArgsConstructor
 @Entity
 
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
