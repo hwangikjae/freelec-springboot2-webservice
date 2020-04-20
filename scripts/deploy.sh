674d57e5d1071989a75e236d0d9e685ee9a57a10
hwangikjae <hwangikjae@nate.com>
2020-04-20 20:20:15 +0900

    다시시작4

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/step2
+PROJECT_NAME=freelec-springboot2-webservice
+
+echo "> Build 파일 복사"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
@@ -28,6 +34,4 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-    -Dspring.profiles.active=real \
-    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \-Dspring.profiles.active=real \$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
